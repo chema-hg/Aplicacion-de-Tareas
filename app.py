311d99a2ff4ac098788977caadbee36bb3490457
Chema HG <honrajm@yahoo.es>
2020-05-26 20:04:18 +0200

    Creando la base de datos

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+# modulo para gestionar sql sin tener que usar directamente instrucciones sql sino de python
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)  # Esta es la aplicación de servidor
+# tenemos que configurar donde se encuentra la base de datos que para sqlite es SQLALCHEMY_DATABASE_URI
+# Para otras bases de datos hay que consultar la documentación en google sqlalchemy database uri
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
 
+# crea una instancia o cursor de la base de datos donde puedo hacer consultas, lo llamare db
+# Pero aun asi tenemos que crear la base de datos que utilizaremos mysql, sqlite3 etc
+# usaremos sqlite3 aparte para crearla solamente (lo hice aparte)
+db = SQLAlchemy(app)
 
 # vista inicial nada mas entrar al navegador
 
