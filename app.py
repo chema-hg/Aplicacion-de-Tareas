6fd5e110bfdee9196cafcdb76635c3d227984b74
Chema HG <honrajm@yahoo.es>
2020-05-28 12:31:24 +0200

    diseñando el formulario

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+
 # modulo para gestionar sql sin tener que usar directamente instrucciones sql sino de python
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)  # Esta es la aplicación de servidor
+
 # tenemos que configurar donde se encuentra la base de datos que para sqlite es SQLALCHEMY_DATABASE_URI
 # Para otras bases de datos hay que consultar la documentación en google sqlalchemy database uri
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
-
 # crea una instancia o cursor de la base de datos donde puedo hacer consultas, lo llamare db
 # Pero aun asi tenemos que crear la base de datos que utilizaremos mysql, sqlite3 etc
 # usaremos sqlite3 aparte para crearla solamente (lo hice aparte)
 db = SQLAlchemy(app)
+
 
 # vista inicial nada mas entrar al navegador
 
 
 @app.route("/")
 def home():
-    return "<h1>Bienvenido al servidor</h1><br><hr>"
+    return render_template('index.html') 
+    # PARA QUE SE RENDERICEN LAS PLANTILLAS TIENEN QUE ESTAR EN EL DIRECTORIO <TEMPLATES>
+    # EN EL DIRECTORIO STATIC ESTARAN LAS IMAGENES CSS ETC, TODO LO ESTATICO
 
 
 if __name__ == "__main__":
     # debug=True para que cada vez que cambiemos algo el servidor se reinice por si solo
     app.run(debug=True)
+
+# ejemplo estraido de https://www.youtube.com/watch?v=V9VU1g4IWlg
